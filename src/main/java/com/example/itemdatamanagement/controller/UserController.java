1e88e749b56096fd764399144a19ea8b4067776a
Suzuka Hara <nm_oo2@yahoo.co.jp>
2024-01-26 09:15:09 +0900

    管理者機能の実装

--- src/main/java/com/example/itemdatamanagement/controller/UserController.java
+++ src/main/java/com/example/itemdatamanagement/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.itemdatamanagement.controller;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,10 +11,13 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.itemdatamanagement.domain.User;
 import com.example.itemdatamanagement.form.InsertUserForm;
+import com.example.itemdatamanagement.form.UpdateUserForm;
 import com.example.itemdatamanagement.service.UserService;
+import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -39,12 +44,13 @@ public class UserController {
 
         User user = new User();
         BeanUtils.copyProperties(form, user);
+        user.setAuthority(0);
 
         userService.insertUser(user);
         return "user/login";
     }
 
-    @GetMapping("/toPageUserLogin")
+    @GetMapping("/login")
     public String toPageUserLogin() {
         return "user/login";
     }
@@ -59,6 +65,40 @@ public class UserController {
             session.setAttribute("user", user);
             return "redirect:/findAll";
         }
+    }
 
+    @GetMapping("/showUserList")
+    public String showUserList(Model model) {
+        List<User> userList = userService.findAllUser();
+        model.addAttribute("userList", userList);
+        return "administrator/list";
+    }
+
+    @GetMapping("/showUserDetail")
+    public String showUserDetail(Integer id, Model model) {
+        User user = userService.findUserById(id);
+        model.addAttribute("user", user);
+        return "administrator/detail";
+    }
+
+    @GetMapping("/toPageUserEdit")
+    public String toPageUserEdit(Integer id, Model model) {
+        User user = userService.findUserById(id);
+        model.addAttribute("user", user);
+        return "administrator/edit";
+    }
+
+    @PostMapping("/deleteUser")
+    public String deleteUser(Integer id) {
+        userService.deleteUser(id);
+        return "redirect:/showUserList";
+    }
+
+    @PostMapping("/updateUser")
+    public String updateUser(UpdateUserForm form) {
+        User user = new User();
+        BeanUtils.copyProperties(form, user);
+        userService.updateUser(user);
+        return "redirect:/showUserList";
     }
 }
